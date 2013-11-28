3f4fb27119d9e17a9ddc6ef55a3d4d243f94e918
tommybotten <tommybjensen@gmail.com>
2013-11-28 19:38:07 +0100

    cleaning up after aircraft controller name changes

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Migrationtest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  get "/" => "loads#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
