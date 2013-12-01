a67dcc10ada4507fe1c0ec71790b97419cf75a1c
tommybotten <tommybjensen@gmail.com>
2013-12-01 11:06:37 +0100

    Added licenses. Fixing a bit on the views for jumpers

--- app/controllers/jumpers_controller.rb
+++ app/controllers/jumpers_controller.rb
@@ -25,7 +25,7 @@ class JumpersController < ApplicationController
   # POST /jumpers.json
   def create
     @jumper = Jumper.new(jumper_params)
-
+    binding.pry
     respond_to do |format|
       if @jumper.save
         format.html { redirect_to @jumper, notice: 'Jumper was successfully created.' }
@@ -69,6 +69,6 @@ class JumpersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def jumper_params
-      params.require(:jumper).permit(:name, :license, :rating, :description, :club, :phone, :email, :verified, :nextofkin, :license, :licensenumber)
+      params.require(:jumper).permit(:name, :license, :rating, :description, :phone, :email, :verified, :nextofkin, :license, :licensenumber, :ratings, clubs: [:id])
     end
 end
--- app/models/jumper.rb
+++ app/models/jumper.rb
@@ -3,4 +3,14 @@ class Jumper < ActiveRecord::Base
   has_and_belongs_to_many :clubs
   has_and_belongs_to_many :ratings
   has_many :slots
+
+
+  class << self
+    # FIXME: Placeholder for viewing the loads that are currently active.
+    # This means: Todays loads that does not have the status "landed"
+    def licenses
+      return ["E","A","B","C","D"]
+    end
+  end
+
 end
