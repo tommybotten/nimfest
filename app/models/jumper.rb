744913424a6439b0dc48641cfbc241c76a01f530
tommybotten <tommybjensen@gmail.com>
2013-12-17 22:36:55 +0100

    Addded a bit info for each jumper

--- app/models/jumper.rb
+++ app/models/jumper.rb
@@ -4,7 +4,19 @@ class Jumper < ActiveRecord::Base
   has_and_belongs_to_many :ratings
   has_many :slots
 
+  def outstanding_payments
+    self.slots.where.not(:paid)
+  end
 
+  def outstanding_amount
+    amount = 0
+    self.outstanding_payments.each do |payment|
+      amount = payment.price + amount
+    end
+    return amount
+  end
+
+ 
   class << self
 		# Available licenses
     def licenses
