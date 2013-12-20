3986a8419903a0ad6c6ebefc3028d66085a7e234
root <espen11@gmail.com>
2013-12-20 16:02:50 +0100

    added top10 stats

--- app/controllers/settlements_controller.rb
+++ app/controllers/settlements_controller.rb
@@ -5,6 +5,8 @@ class SettlementsController < ApplicationController
     @total_outstanding = Slot.total_not_paid
     # Should be restricted to todays load
     @loads = Load.all
+    @slots = Slot.find(:all, :select => 'count(*) count, jumper_id', :group => 'jumper_id', :order => 'count DESC', :limit => 5)
+		@jumpers = Jumper.all
   end
 
 end
