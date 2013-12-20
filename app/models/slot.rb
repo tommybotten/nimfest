de171c8ba480361f0d8a3d114496e9edbfee1a76
Tommy Botten Jensen <tommybjensen@gmail.com>
2013-12-20 21:06:22 +0100

    Added all (norwegian) jumpypes

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -6,7 +6,23 @@ class Slot < ActiveRecord::Base
   class << self
 		# Available jump types
     def jumptypes
-      return ["Treningshopp","Demo","FS-Utsjekk"]
+      return ["Treningshopp",
+						"Demo",
+						"FS-Utsjekk",
+						"UL",
+						"UL-M",
+						"UL-T",
+						"FF-5 sekunder",
+						"FF-7 sekunder",
+						"FF-10 sekunder",
+						"FF-15 sekunder",
+						"FF-20 sekunder",
+						"FF-30 sekunder",
+						"FS-Utsjekk",
+						"Wingsuit",
+						"Hop and pop",
+						"Fjellflyving"
+						]
     end
   end
 end
