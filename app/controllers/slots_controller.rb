e95d476a88ce4b788421fa7373c2c42ec8ae0e84
Tommy Botten Jensen <tommybjensen@gmail.com>
2013-12-28 21:21:24 +0100

    Tie together loads and slots. Needs some ajax magic to be user friendly

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -69,6 +69,6 @@ class SlotsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def slot_params
-      params.require(:slot).permit(:height, :price, :paid, :approved, :jumptype, :jumpmaster)
+      params.require(:slot).permit(:height, :price, :paid, :approved, :jumptype, :jumpmaster, :jumper_id, :load_id)
     end
 end
