3af65322ecb150b70cd15fbeb03a3fa83175c4fb
root <espen11@gmail.com>
2013-12-30 00:52:58 +0100

    added links to items in overview. only todays load shows in loads and overview

--- app/controllers/settlements_controller.rb
+++ app/controllers/settlements_controller.rb
@@ -5,7 +5,7 @@ class SettlementsController < ApplicationController
     @total_outstanding = Slot.total_not_paid
     # Should be restricted to todays load
     @loads = Load.all
-    @slots = Slot.find(:all, :select => 'count(*) count, jumper_id', :group => 'jumper_id', :order => 'count DESC', :limit => 5)
+    @slots = Slot.find(:all, :select => 'count(*) count, jumper_id', :group => 'jumper_id', :order => 'count DESC', :limit => 10)
 		@jumpers = Jumper.all
   end
 
