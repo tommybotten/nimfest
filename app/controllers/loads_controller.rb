2b468bb41dac08252a30bfcd05714704d3f0d080
root <espen11@gmail.com>
2013-12-30 01:43:16 +0100

    Fixed the double-booking bug for slots, and small var name change

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -27,6 +27,14 @@ class LoadsController < ApplicationController
     @slots = @load.slots
 		@slot = Slot.new
 		@aircraft = @load.aircraft
+
+		avail_jumpers ||= []
+		used_jumpers ||= []
+		slots = @load.slots.all
+		slots.each do |slot|
+			used_jumpers << slot.jumper
+		end
+		@avail_jumpers = Jumper.all - used_jumpers
   end
 
   # POST /loads
--- app/controllers/settlements_controller.rb
+++ app/controllers/settlements_controller.rb
@@ -5,7 +5,7 @@ class SettlementsController < ApplicationController
     @total_outstanding = Slot.total_not_paid
     # Should be restricted to todays load
     @loads = Load.all
-    @slots = Slot.find(:all, :select => 'count(*) count, jumper_id', :group => 'jumper_id', :order => 'count DESC', :limit => 10)
+    @ff_top_ten = Slot.find(:all, :select => 'count(*) count, jumper_id', :group => 'jumper_id', :order => 'count DESC', :limit => 10)
 		@jumpers = Jumper.all
   end
 
